06de057c62001b552c940c02f23523b40473dfb6
orfeas.aidonopo <orfeas.aidonopo@hevs.ch>
2020-02-21 14:27:49 +0100

    modify setup file, name found for the service: Melampus

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-orfi2017", # Replace with your own username
+    name="quantimage-melampus", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Orfeas Aidonopoulos",
+    author_email="orfeas.aidonopoulos@hevs.ch",
+    description="Melampus: Machine learning package for Imagine infrastracture.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/orfi2017/imagine_ml_infra/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD 3 License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
