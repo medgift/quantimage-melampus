a926d2c2c6d967cab8a6156d9250757742272615
orfeas.aidonopo <orfeas.aidonopo@hevs.ch>
2020-03-13 13:43:29 +0100

    requirements modified and init file added

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 3 License",
+        "License :: OSI Approved",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
