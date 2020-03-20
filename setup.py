b61da408eab57b30982cc0ec6687cb41ac028a44
orfeas.aidonopo <orfeas.aidonopo@hevs.ch>
2020-03-20 04:30:58 +0200

    gitignore and setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="melampus", # Replace with your own username
+    name="quantimage-melampus", # Replace with your own username
     version="0.0.2",
     author="Orfeas Aidonopoulos",
     author_email="orfeas.aidonopoulos@hevs.ch",
