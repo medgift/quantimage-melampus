6049d91317d29320d470f7143085de6ead8f29fc
orfeas.aidonopo <orfeas.aidonopo@hevs.ch>
2020-05-06 23:28:14 +0300

    update docstrings in feature selector

--- melampus/feature_selector.py
+++ melampus/feature_selector.py
@@ -13,6 +13,7 @@ class MelampusFeatureSelector(MelampusPreprocessor):
     def variance_threshold(self, p_val=None):
         '''
         It removes all features whose variance doesn’t meet some threshold. By default, it removes all zero-variance features
+
         :param p_val: p_value for defining the threshold. default value: 0.8
         :return: transormed array of removed correlated features
         '''
