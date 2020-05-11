1f9c94d4a75b0568b156c4f0a50c47758d743e4c
orfeas.aidonopo <orfeas.aidonopo@hevs.ch>
2020-05-11 14:42:45 +0300

    Survival Analyzer ready (classic (2 options) and regression analysis)

--- melampus/survival_analysis.py
+++ melampus/survival_analysis.py
@@ -1,17 +1,18 @@
 import pandas as pd
-from lifelines import KaplanMeierFitter, NelsonAalenFitter
+from lifelines import KaplanMeierFitter, NelsonAalenFitter, CoxPHFitter
 
 
-class MelampusSurvivalAnalysis:
-    def __init__(self, data: pd.DataFrame, time_column: str, event_column: str, method='kaplan_meier'):
+class MelampusSurvivalAnalyzer:
+    def __init__(self, data: pd.DataFrame, time_column: str, event_column: str, method='cox_model'):
         self.data = data
         self.time_column = time_column
         self.event_column = event_column
         self.method = method
         self.T = pd.Series  # time variable
         self.E = pd.Series  # event variable
-        self.classifier = object
-        self.init_columns()
+        self.analyzer = object
+        if self.method != 'cox_model':
+            self.init_columns()
         self.init_survival_classifier()
 
     def init_columns(self):
@@ -23,17 +24,25 @@ class MelampusSurvivalAnalysis:
 
     def init_survival_classifier(self):
         if self.method == 'kaplan_meier':
-            self.classifier = KaplanMeierFitter()
+            self.analyzer = KaplanMeierFitter()
         elif self.method == 'nelson_aalen':
-            self.classifier = NelsonAalenFitter()
+            self.analyzer = NelsonAalenFitter()
+        elif self.method == 'cox_model':
+            self.analyzer = CoxPHFitter()
         else:
-            self.classifier = KaplanMeierFitter()
+            self.analyzer = CoxPHFitter()
 
     def train(self):
-        try:
-            self.classifier.fit(self.T, self.E)
-        except Exception as e:
-            raise Exception(str(e))
+        if self.method == 'cox_model':
+            try:
+                self.analyzer.fit(self.data, duration_col = self.time_column, event_col= self.event_column)
+            except Exception as e:
+                raise Exception(str(e))
+        else:
+            try:
+                self.analyzer.fit(self.T, self.E)
+            except Exception as e:
+                raise Exception(str(e))
 
     def assess(self):
-        pass
+        return self.analyzer.concordance_index_
