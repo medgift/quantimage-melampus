da733da65699cbb4ea06bef022572611d4a3cf74
Roger Schaer <roger.schaer@hevs.ch>
2020-08-04 13:30:09 +0200

    Fix error in survival analysis constructor

--- melampus/survival_analysis.py
+++ melampus/survival_analysis.py
@@ -20,7 +20,7 @@ class MelampusSurvivalAnalyzer(MelampusPreprocessor):
 
     def __init__(self, filename: str, time_column: str, event_column: str):
         # Change the initialization of data, as we don't have a single outcomes array like for classification
-        self.data = pd.read_csv(self.filename)
+        self.data = pd.read_csv(filename)
         self.time_column = time_column
         self.event_column = event_column
         self.T = pd.Series  # time variable
